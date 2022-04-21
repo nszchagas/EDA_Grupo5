8bcc16077db9c8f73413abf7e95d4a16f9301aa3
PedroHhenriq <100311889+PedroHhenriq@users.noreply.github.com>
2022-04-20 21:07:15 -0300

    Add files via upload

--- avl.c
+++ avl.c
@@ -2,10 +2,27 @@
 #include <stdlib.h>
 #include "arquivos.h"
 #include "estruturas.h"
+/**
+* Objetivo: Arvore binaria balanceada
+* Entrada: Opcao escolhida pelo usuario de arquivo
+* Saida: Arvore binaria e fator de balanceamento
+*/
+            /*
 
+                    O QUE ESTA FALTANDO?
+                    >> Calcular o fator de balanceamento na opcao 2
+                    >> Imprimir fator de balanceamento na opcao 3
+                    >> Desalocar os nos na opcao 4
+
+                    IMPORTANTE:
+                    Nao mudar o codigo. Se quiser acrescentar algo,
+                    crie uma funcao separada em estruturas.c
+                    e a partir dali mude o codigo
+
+            */
 int main(){
     // Declaracoes
-    char endereco_arquivo[30]; // Ponteiro para endereï¿½o do arquivo
+    char endereco_arquivo[30]; // Ponteiro para endereço do arquivo
     FILE *arquivo_csv = NULL;
     int aux, opcao, valor;
     No *raiz = NULL;
@@ -13,14 +30,14 @@ int main(){
     aux = 0, valor = 0;
     // Sinal onde goto deve retornar
     inicio:
-    // Limpando a tela apï¿½s retorno
+    // Limpar a tela após retorno
     if(opcao < 1 && opcao > 5 || opcao >= 1){
         getch();
         limpar();
     }
-    menu(); // Chamando menu de escolhas
+    menu(); // Chamar menu de escolhas
     scanf("%d", &opcao);
-    // Limpando a tela
+    // Limpar a tela
     limpar();
     switch(opcao){
         /** Concluido. Nao alterar. */
@@ -30,14 +47,12 @@ int main(){
             scanf("%d", &opcao);
             switch(opcao){
                 case 1:
-                    raiz = reiniciar(raiz);
                     printf("Digite o endereco do arquivo a ser aberto: ");
                     scanf("%s", endereco_arquivo);
                     arquivo_csv = abrir_arquivo_leitura(endereco_arquivo);
                     goto continua;
                     break;
                 case 2:
-                    raiz = reiniciar(raiz);
                     criar_arquivo_aleatorio();
                     arquivo_csv = abrir_arquivo_leitura("numeros.csv");
                     goto continua;
@@ -50,13 +65,13 @@ int main(){
             }
             // Sinal 2 onde o goto deve continuar
             continua:
-            // Avaliando arquivo aberto e gerar arvore
+            // Avaliar arquivo aberto e gerar arvore
             if(arquivo_csv == NULL){
-                //Nï¿½o aberto
+                //Não aberto
                 printf("Erro! Arquivo %s nao pode ser aberto.\n", endereco_arquivo);
             }else{
                 rewind(arquivo_csv);
-                // Inserindo todos os numeros do arquivo na arvore
+                // Inserir todos os numeros do arquivo na arvore
                 while((aux = fscanf(arquivo_csv, "%d", &valor) != EOF)){
                     raiz = inserir(raiz, valor);
                     fseek(arquivo_csv, 1, SEEK_CUR);
@@ -64,24 +79,18 @@ int main(){
             }
             goto inicio;
             break;
+        /** Nao feito */
         case 2:
-            printf("Calculando fatores de balanceamento: \n \n");
-            fatorBalanceamento(raiz);
-            printf("Calculo: \n");
-            printf("[Chave] (No da esquerda) - (No da direita) = (Fator de Balanceamento) \n \n");
-            imprimirCalculo(raiz);
+            /* funcao() */
             goto inicio;
             break;
+        /** Falta imprimir os fatores de balanceamento */
         case 3:
-            printf("- - - - - - - - - - - - - - - - - - - - - - \n");
-            printf("Arvore Binaria: \n");
-            imprimirArvore(raiz, 1); // Imprime a arvore binaria
-            printf("\n");
-            printf("- - - - - - - - - - - - - - - - - - - - - - \n");
-            printf("Chaves e Fator de Balanceamento: \n");
-            imprimir(raiz);
+            imprimir(raiz, 1); // Imprimir a arvore binaria
+            /* funcao() */
             goto inicio;
             break;
+        /** A funcao liberarNo nao funciona */
         case 4:
             liberaNo(raiz);
             if (raiz == NULL){
